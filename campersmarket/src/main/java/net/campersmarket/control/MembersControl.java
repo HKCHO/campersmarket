3b73290d6ef9136cd00a23a2afb4980e7718e693
HKCHO <j880825@gmail.com>
2015-05-03 18:48:00 +0900

    오랜만에 커밋~!

--- campersmarket/src/main/java/net/campersmarket/control/MembersControl.java
+++ campersmarket/src/main/java/net/campersmarket/control/MembersControl.java
@@ -1,22 +1,15 @@
 package net.campersmarket.control;
 
-import java.util.HashMap;
-
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import net.campersmarket.domain.Members;
-import net.campersmarket.service.MembersService;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.google.gson.JsonObject;
-
-@Controller("/membersControl")
+@Controller("membersControl")
 @RequestMapping("/members")
 public class MembersControl {
 	static Logger log = Logger.getLogger(MembersControl.class);
@@ -28,10 +21,6 @@ public class MembersControl {
 	  public void add(HttpServletRequest request,HttpServletResponse response) throws Exception {
 		System.out.println("컨트롤러 드러옴");
 		
-		
-	  	
-	  	
-	  	
 //	  	HashMap<String, Object> resultMap = new HashMap<>();
 //	  	resultMap.put("status","success");
 //	  	
--- campersmarket/src/main/webapp/contents/carpool/cp_js/search.js
+++ campersmarket/src/main/webapp/contents/carpool/cp_js/search.js
@@ -1,31 +1,46 @@
 $(function(){
 	
-	$('#signinBtn').click(function(){
+	$('#carpoolRegBtn').click(function(){
 		
 		console.log("가입버튼 누름");
 	  
-	  $.post('../../membersControl/members/add.do' //  URL 
-	      , {  //서버에 보낼 데이터를 객체에 담아 넘긴다 
-	        position : $('#rank').val(),     //직급 
-	        name : $('#name').val(),  //이름
-	        email : $('#email').val(),   //이메일 
-	        phone : $('#phone').val()  //폰번호 
-	      } 
-	      , function(result){ // 서버로부터 응답을 받았을 때 호출될 메서드
-	        if (result.status == "success") {
-	          alert("성공!! ");
-	          location.href="/staffmanager/main.html";
-	          
-	        } else {
-	          alert("등록 실패!");
-	        }
-	      }
-	      , 'map' // 서버가 보낸 데이터를 JSON 형식으로 처리
-	      )
-	    //서버 요청이 실패했을 때 호출될 함수 등록   
-	   .fail(function(jqXHR, textStatus, errorThrown){ 
-	     alert(textStatus + ":" + errorThrown);
-	   });
+//	  $.post('../../membersControl/members/add.do' //  URL 
+//	      , {  //서버에 보낼 데이터를 객체에 담아 넘긴다 
+//	        position : $('#rank').val(),     //직급 
+//	        name : $('#name').val(),  //이름
+//	        email : $('#email').val(),   //이메일 
+//	        phone : $('#phone').val()  //폰번호 
+//	      } 
+//	      , function(result){ // 서버로부터 응답을 받았을 때 호출될 메서드
+//	        if (result.status == "success") {
+//	          alert("성공!! ");
+//	          location.href="/staffmanager/main.html";
+//	          
+//	        } else {
+//	          alert("등록 실패!");
+//	        }
+//	      }
+//	      , 'map' // 서버가 보낸 데이터를 JSON 형식으로 처리
+//	      )
+//	    //서버 요청이 실패했을 때 호출될 함수 등록   
+//	   .fail(function(jqXHR, textStatus, errorThrown){ 
+//	     alert(textStatus + ":" + errorThrown);
+//	   });
+		$.ajax({
+      url: '../../membersControl/members/add.do',
+      data: {
+		      		position : $('#rank').val(),      //직급 
+				      name : $('#name').val(),  				//이름
+				      email : $('#email').val(),		    //이메일 
+				      phone : $('#phone').val()
+		      	},
+      success: function(result){
+      	alert("success");
+      },
+      error: function( jqXhr, textStatus, errorThrown ){
+          console.log( errorThrown );
+      }
+  });
 	  
 	}); 
 	
